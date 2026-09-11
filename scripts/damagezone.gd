132142bf943c72e58190fd3490a574c144b90c3a
Artur <artur.pawel.pietrzyk@gmail.com>
2026-09-11 16:20:02 +0100

    basic abilities implemented that only work for the specific element you are in

--- scripts/damagezone.gd
+++ scripts/damagezone.gd
@@ -6,5 +6,6 @@ func _on_body_entered(body: Node2D) -> void:
 	# Call take_damage on the player if available
 	if body.has_method("take_damage"):
 		body.take_damage(damage)
+
 	elif body.get_parent() and body.get_parent().has_method("take_damage"):
 		body.get_parent().take_damage(damage)
